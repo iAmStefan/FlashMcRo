8e7c8bc467d616d5c681c4734205b7a69ebebe78
Taloi Stefan <taloistefanel8@gmail.com>
2019-09-14 21:42:52 +0300

    Code organization + new topic overlay

---
+++ static/javascript/topic.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.newTopic').click(function() {
+        $('.overlay').show();
+    });
+
+   /* $('html').click(function() {
+        $('.overlay').hide();
+    });*/
+});
